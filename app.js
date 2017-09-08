a0529224fdce248271fb6c9d3f5ada1231a76c7e
moeabdol <mohd.a.saed@gmail.com>
2017-09-08 14:44:08 +0300

    Implement basic users routes and controller

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
 
-const express    = require('express');
-const bodyParser = require('body-parser');
-const cors       = require('cors');
-const session    = require('express-session');
-const morgan     = require('morgan');
-const mongoose   = require('mongoose');
-const config     = require('./config');
-const app        = express();
+const express     = require('express');
+const app         = express();
+const bodyParser  = require('body-parser');
+const cors        = require('cors');
+const session     = require('express-session');
+const morgan      = require('morgan');
+const mongoose    = require('mongoose');
+const config      = require('./config');
+const usersRoutes = require('./routes/users');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.db, { useMongoClient: true }, (err) => {
@@ -19,6 +20,8 @@ mongoose.connect(config.db, { useMongoClient: true }, (err) => {
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/api', usersRoutes);
 
 app.get('*', (req, res) => {
   res.send('Hello, World!');
---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+const index = (req, res) => {
+  res.send('testing GET /api/users');
+};
+
+module.exports = {
+  index
+};
